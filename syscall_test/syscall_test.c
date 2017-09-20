b83f98f357e025945f27ecf21215de106c86bb87
Chase E Stewart <chst9148@colorado.edu>
2017-09-20 00:50:13 -0600

    improve syscall, syscall_test, makefile

--- syscall_test/syscall_test.c
+++ syscall_test/syscall_test.c
@@ -8,11 +8,62 @@
 
 int main()
 {
-	int32_t input_buffer[BUF_SIZE] = {1,2354,2132,45,12,7,3,9,3,10000};
-	int32_t *output_buffer;
-	output_buffer = (int32_t *) malloc(sizeof(int32_t *) * BUF_SIZE);
+	int idx;
+	const int32_t input_buffer[BUF_SIZE] = {5,3,4,8,5,9,1,2,3,0} ; 
+	const int32_t *output_buffer;
 
-	syscall(333, input_buffer, BUF_SIZE, output_buffer);
+	/* create input buffer */
+	//input_buffer = (int32_t *) malloc(sizeof(int32_t) * BUF_SIZE);
+	//if(!input_buffer)
+	//{
+	//	printf("ERROR: Couldn't allocate input buff\n");
+	//}
+	//
+	///* fill input buffer */
+	//for (idx=0; idx<BUF_SIZE; idx++)
+	//{
+	//	*(input_buffer+idx) = rand();
+	//}
+	
+	printf("INPUT_BUFFER START [ %d", input_buffer[0]);
+	for(idx=1; idx<BUF_SIZE; idx++)
+	{
+		printf(", %d", input_buffer[idx]);
+	}
+	printf("]\n");
 	
 
+
+	/* create output buffer */
+	output_buffer = (int32_t *) malloc(sizeof(int32_t) * BUF_SIZE);
+	if(!output_buffer)
+	{
+		printf("ERROR: Couldn't allocate ouput buff\n");
+	}
+	printf("OUTPUT_BUFFER START [ %d", output_buffer[0]);
+	for(idx=1; idx<BUF_SIZE; idx++)
+	{
+		printf(", %d", output_buffer[idx]);
+	}
+	printf("]\n");
+
+
+	/* Run the syscall */
+	syscall(333, input_buffer, BUF_SIZE, output_buffer);
+
+
+	/* print the results */
+	printf("[ %d", input_buffer[0]);
+	for(idx=1; idx<BUF_SIZE; idx++)
+	{
+		printf(", %d", input_buffer[idx]);
+	}
+	printf("]\n");
+
+	printf("[ %d", output_buffer[0]);
+	for(idx=1; idx<BUF_SIZE; idx++)
+	{
+		printf(", %d", output_buffer[idx]);
+	}
+	printf("]\n");
 }
--- system_call/mysort.c
+++ system_call/mysort.c
@@ -1,24 +1,3 @@
-/*****************************************************
- * Redistribution, modification or use of this software in source or binary forms 
- * is permitted as long as the files maintain this copyright. Users are permitted to 
- * modify this and use it to learn about the field of embedded software but don't copy 
- * my (Chase E Stewart's) work for class, I worked really hard on this. Alex Fosdick and 
- * the University of Colorado and Chase E Stewart are not liable for any misuse of this material. 
- * License copyright (C) 2017 originally from Alex Fosdick, code by Chase E Stewart.
- *****************************************************/
-/**
- * @file system_call.c
- * @brief A system call that sorts an int32_t buffer
- * 
- * The system call is called sys_sort, it sorts an input array of chars up to the provided length,
- * and then returns them via another input pointer
- *
- * @author Chase E Stewart
- * @date Sept 19 2017
- * @version 1.0
- *
- */
-
 #include <linux/kernel.h>
 #include <linux/syscalls.h>
 #include <linux/capability.h>
@@ -30,33 +9,26 @@
 #include <asm-generic/errno.h>
 #include <asm-generic/errno-base.h>
 
-#define SYSCALL_NO 333
+#define SYSCALL_NO 333 /* just FYI */
 
-/*
- * @brief take inputs, check them for correctness, sort them, then pass them back
- *
- * @param int32_t *buffer - an input buffer hopefully of size "buffer_size"
- * @param uint32_t buffer_size - length of the buffer to sort
- * @param int32_t *sort_buffer - a pointer to the now-sorted buffer
- *
- * @return none
- */
-SYSCALL_DEFINE3(sys_sort, int32_t*, buffer, uint32_t, buffer_size, int32_t*, sort_buffer)
+
+SYSCALL_DEFINE3(mysort, int32_t*, buffer, size_t, buffer_size, int32_t*, sort_buffer)
 {
+	int idx = 0;
 	int retval          = 0; /* return value   */
 	uint32_t first_idx  = 0; /* outer iterator */
 	uint32_t second_idx = 0; /* inner iterator */
 	int32_t  temp       = 0; /* swap variable  */
-	uint32_t *kbuffer = NULL;
+	int32_t *kbuffer = NULL; /* internal kernel buffer */
 
 	/* check all inputs for validity */
 	printk(KERN_NOTICE "[sys_sort] INVOKING SYSCALL SYS_SORT\n");
 
-	if (!access_ok(VERIFY_WRITE,buffer,buffer_size))
-	{
-		return EACCES; /* error out if out of userspace */
-	}
-	else if (!buffer)
+	//if (!access_ok(VERIFY_WRITE,buffer,buffer_size))
+	//{
+	//	return EACCES; /* error out if out of userspace */
+	//}
+	if (!buffer)
 	{
 		printk(KERN_ERR "[sys_sort] INVALID INPUT BUFFER \n");
 		return EINVAL; /* invalid input buffer */ 
@@ -68,12 +40,24 @@ SYSCALL_DEFINE3(sys_sort, int32_t*, buffer, uint32_t, buffer_size, int32_t*, sor
 	}
 
 	/* Create a buffer in the kernel using kmalloc */
-	kbuffer = (uint32_t *) kmalloc (sizeof(uint32_t) * buffer_size, GFP_KERNEL);
+	kbuffer = (int32_t *) kmalloc (sizeof(int32_t) * buffer_size, GFP_KERNEL);
 	if (!kbuffer)
 	{
 		printk(KERN_ERR "[sys_sort] UNABLE TO MALLOC K_BUFFER \n");	
 		return ENOMEM; /* no memory to complete a malloc */
 	}
+	else
+	{
+		printk(KERN_NOTICE "[sys_sort] MALLOC K_BUFFER OF SIZE %d \n", buffer_size);	
+	}
+        printk("[ %d", kbuffer[0]);
+        for(idx=1; idx<buffer_size; idx++)
+        {
+                printk(", %d", kbuffer[idx]);
+        }
+        printk("]\n");
+
+
 	if (!capable(CAP_SYS_ADMIN))
 	{
 		printk(KERN_ERR "[sys_sort] CAP_SYS_ADMIN RETURNED ERROR - USE SUDO\n");	
@@ -88,6 +72,14 @@ SYSCALL_DEFINE3(sys_sort, int32_t*, buffer, uint32_t, buffer_size, int32_t*, sor
 		return retval; /* use the retval from copy_from_user */
 	}
 	
+        printk("[ %d", kbuffer[0]);
+        for(idx=1; idx<buffer_size; idx++)
+        {
+                printk(", %d", kbuffer[idx]);
+        }
+        printk("]\n");
+
+
 	
 	/* now sort the buffer with bubble sort*/
 	printk(KERN_NOTICE "[sys_sort] STARTING BUBBLE SORT\n");
@@ -106,12 +98,19 @@ SYSCALL_DEFINE3(sys_sort, int32_t*, buffer, uint32_t, buffer_size, int32_t*, sor
 		}
 	}
 	printk(KERN_NOTICE "[sys_sort] COMPLETED BUBBLE SORT\n");
+        
+	printk("[ %d", kbuffer[0]);
+        for(idx=1; idx<buffer_size; idx++)
+        {
+                printk(", %d", kbuffer[idx]);
+        }
+        printk("]\n");
 
 	/* return result of sort to user-space and exit*/
 	retval = copy_to_user(sort_buffer, kbuffer, buffer_size);
 	if (retval != 0)
 	{
-		printk(KERN_ERR "[sys_sort] UNABLE TO COPY BACK TO USER BUFFER \n");	
+		printk(KERN_ERR "[sys_sort] UNABLE TO COPY BACK TO USER BUFFER; ERRNO %d  \n", retval);	
 		return retval; /* use the retval from copy_to_user */
 	}
 	
