caa0e2570ac0890334674e926af2b376f749f621
Chase E Stewart <chst9148@colorado.edu>
2017-09-19 22:15:11 -0600

    wasn't mallocing output buffer

--- syscall_test/syscall_test.c
+++ syscall_test/syscall_test.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/syscall.h>
 #include <unistd.h>
 
@@ -9,6 +10,7 @@ int main()
 {
 	int32_t input_buffer[BUF_SIZE] = {1,2354,2132,45,12,7,3,9,3,10000};
 	int32_t *output_buffer;
+	output_buffer = (int32_t *) malloc(sizeof(int32_t *) * BUF_SIZE);
 
 	syscall(333, input_buffer, BUF_SIZE, output_buffer);
 	
